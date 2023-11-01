6dd0a4f7b9df1a57fed4f22d7fcda58b0cc35dd9
Padma Patil <padma@email.com>
2023-11-01 14:44:22 -0700

    initial js

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function hideResults() {
+    document.getElementById("ent21").setAttribute("class", "hidden");
+    document.getElementById("ent25").setAttribute("class", "hidden");
+    document.getElementById("ent35").setAttribute("class", "hidden");
+    document.getElementById("ent20").setAttribute("class", "hidden");
+}
+
