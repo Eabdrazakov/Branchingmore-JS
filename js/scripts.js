01e04e35e3c8ae8d810cfc97fec1842943ee4513
Padma Patil <padma@email.com>
2023-11-01 15:06:01 -0700

    Add js

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,24 @@ function hideResults() {
     document.getElementById("ent20").setAttribute("class", "hidden");
 }
 
+window.onload = function () {
+    document.querySelector("form").onsubmit = function (event) {
+        event.preventDefault();
+        hideResults();
+        const age = parseInt(document.querySelector("input#age").value);
+        const height = parseInt(document.querySelector("input#height").value);
+
+        if (age >= 35 && height >= 55) {
+            document.getElementById("ent21").removeAttribute("class");
+            document.getElementById("ent25").removeAttribute("class");
+            document.getElementById("ent35").removeAttribute("class");
+        } else if (age >= 25 || height >= 48) {
+            document.getElementById("ent21").removeAttribute("class");
+            document.getElementById("ent25").removeAttribute("class");
+        } else if (age >= 21) {
+            document.getElementById("ent21").removeAttribute("class");
+        } else {
+            document.getElementById("ent20").removeAttribute("class");
+        }
+    }
+};
